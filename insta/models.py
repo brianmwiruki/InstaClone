485ea3332d0da62b748413460052a8da5a4d725f
Brian Mwiruki <mwiruki@outlook.com>
2020-03-06 13:21:11 +0300

    post model added

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class post(models.Model):
+    author = models.ForeignKey('auth.user',on_delete= models.CASCADE)
+    image = models.ImageField(blank=True, null=True)
+    caption models.TextField()
+    created_date = models.DateTimeField(default= timezone.now)
+    
