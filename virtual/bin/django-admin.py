0b3ebabe7bda3a76362f3beb02eb4aa3a7aaf363
Brian Mwiruki <mwiruki@outlook.com>
2020-03-06 12:51:23 +0300

    forgestagram initialization

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/bryan/Documents/moringa-school-projects/Python/Django/ForgeStagram/virtual/bin/python3.8
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
